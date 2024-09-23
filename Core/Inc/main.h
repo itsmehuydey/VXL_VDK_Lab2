ace1dd59904d6db397901253e0288c180befae17
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-23 10:23:20 +0700

    mainq

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -58,6 +58,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define Led_red_Pin GPIO_PIN_5
+#define Led_red_GPIO_Port GPIOA
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
