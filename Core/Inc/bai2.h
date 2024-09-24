13dfdf687d656ca98ceadc004a37cc4cc6dc8d58
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-24 10:30:45 +0700

    e2

--- Core/Inc/bai2.h
+++ Core/Inc/bai2.h
@@ -7,7 +7,11 @@
 #include "main.h"
 #ifndef INC_BAI2_H_
 #define INC_BAI2_H_
-
+extern int counter7Seg;
+extern int counterDot;
+extern int currentIndex7Seg;
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
+ void display7SEG(int number);
 
 
 #endif /* INC_BAI2_H_ */
