c7d2b6435fa60c11ed46898d85f963eccecec0a7
LAPTOP-NIOUD1BM\pc <huyt24309@gmail.com>
2024-09-26 23:29:37 +0700

    e7

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,6 +56,22 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int timer0_counter = 0;
+int timer0_flag = 0;
+int TIMER_CYCLE = 10;
+
+void setTimer0(int duration) {
+    timer0_counter = duration / TIMER_CYCLE;
+    timer0_flag = 0;
+}
+
+void timer_run() {
+    if(timer0_counter > 0) {
+        timer0_counter--;
+        if(timer0_counter == 0)
+            timer0_flag = 1;
+    }
+}
 
 /* USER CODE END 0 */
 
@@ -94,12 +110,14 @@ HAL_TIM_Base_Start_IT (& htim2 ) ;
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
+	  setTimer0 (1000) ;
+	   while (1) {
+	   if( timer0_flag == 1) {
+	   HAL_GPIO_TogglePin ( Led_red_GPIO_Port , Led_red_Pin ) ;
+	  setTimer0 (1000) ;
+	   }
+	   }
     /* USER CODE BEGIN 3 */
-  }
   /* USER CODE END 3 */
 }
 
@@ -225,6 +243,8 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+
+
 const int MAX_LED = 4;
 int led_buffer[4] = {1, 2, 3, 4};
 
@@ -240,22 +260,22 @@ void update7SEG(int index) {
     switch (index) {
         case 0:
         	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
-            display7SEG(led_buffer[0]);
+            display7SEG(index);
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_RESET);
             break;
         case 1:
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_SET);
-            display7SEG(led_buffer[1]);
+            display7SEG(index);
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, GPIO_PIN_RESET);
             break;
         case 2:
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-            display7SEG(led_buffer[2]);
+            display7SEG(index);
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
             break;
         case 3:
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
-            display7SEG(led_buffer[3]);
+            display7SEG(index);
             HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
             break;
         default:
@@ -284,24 +304,10 @@ void updateClockBuffer () {
 
 void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim )
 {
-    if( counter7Seg <= 0) {
-        counter7Seg = 100;
-        ++index_led;
-        index_led = index_led % 4;
-        update7SEG(index_led);
-    }
-    else {
-        --counter7Seg;
-    }
 
-    if( counterDot <= 0) {
-        counterDot = 100;
-        HAL_GPIO_TogglePin ( GPIOA, GPIO_PIN_4 );
-    }
-    else {
-        --counterDot;
-    }
-}
+ timer_run () ;
+
+ }
 
 
 /* USER CODE END 4 */
